95682cdb77f6777094286e9ae13769527835fb94
Paul Spain <pvspain@gmail.com>
2025-04-02 18:20:18 +1100

    Add Absinthe packages. mix setup.

--- mix.exs
+++ mix.exs
@@ -44,7 +44,10 @@ defmodule PlateSlate.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"}
+
     ]
   end
 
