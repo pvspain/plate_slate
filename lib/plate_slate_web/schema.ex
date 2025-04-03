0a3a93a98c639fa72766fc72098d2073bbaf0831
Paul Spain <pvspain@gmail.com>
2025-04-03 18:53:28 +1100

    Add "matching" filter to MenuItem.name

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -2,10 +2,20 @@ defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
   alias PlateSlate.{Menu, Repo}
+  import Ecto.Query
 
   query do
     field :menu_items, list_of(:menu_item) do
-      resolve(fn _, _, _ -> {:ok, Repo.all(Menu.Item)} end)
+      arg(:matching, :string)
+
+      resolve(fn
+        _, %{matching: name}, _ when is_binary(name) ->
+          query = from t in Menu.Item, where: ilike(t.name, ^"%#{name}%")
+          {:ok, Repo.all(query)}
+
+        _, _, _ ->
+          {:ok, Repo.all(Menu.Item)}
+      end)
     end
   end
 
