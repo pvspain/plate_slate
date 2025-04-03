2fca0eec2eb8fc9ed457891c09674194b6a4f563
Paul Spain <pvspain@gmail.com>
2025-04-03 23:30:51 +1100

    - Refactor MenuItem matching filter anonymous function to Resolver function. - Add menuItems matching tests.

--- lib/plate_slate/menu.ex
+++ lib/plate_slate/menu.ex
@@ -209,9 +209,13 @@ defmodule PlateSlate.Menu do
       [%Item{}, ...]
 
   """
-  def list_items do
-    Repo.all(Item)
+  def list_items(%{matching: name}) when is_binary(name) do
+    Item
+    |> where([m], ilike(m.name, ^"%#{name}%"))
+    |> Repo.all()
   end
+
+  def list_items(_), do: Repo.all(Item)
 
   @doc """
   Gets a single item.
--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -1,21 +1,12 @@
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
-  alias PlateSlate.{Menu, Repo}
-  import Ecto.Query
+  alias PlateSlateWeb.Resolvers
 
   query do
     field :menu_items, list_of(:menu_item) do
       arg(:matching, :string)
-
-      resolve(fn
-        _, %{matching: name}, _ when is_binary(name) ->
-          query = from t in Menu.Item, where: ilike(t.name, ^"%#{name}%")
-          {:ok, Repo.all(query)}
-
-        _, _, _ ->
-          {:ok, Repo.all(Menu.Item)}
-      end)
+      resolve(&Resolvers.Menu.menu_items/3)
     end
   end
 
