9ef07fe29d1f74493c60fce81f5d2cf3c96f99bd
Paul Spain <pvspain@gmail.com>
2025-04-07 14:22:17 +1000

    Add test for query using fragments.

--- test/plate_slate_web/schema/query/search_test.exs
+++ test/plate_slate_web/schema/query/search_test.exs
@@ -14,7 +14,36 @@ defmodule PlateSlateWeb.Schema.Query.SearchTest do
   }
   """
   @variables %{term: "e"}
-  test "search returns a list of menu items and categories" do
+  test "Search returns a list of menu items and categories" do
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+    assert %{"data" => %{"search" => results}} = json_response(response, 200)
+    assert length(results) > 0
+    assert Enum.find(results, &(&1["__typename"] == "Category"))
+    assert Enum.find(results, &(&1["__typename"] == "MenuItem"))
+  end
+
+  @query """
+  query Search($term: String!) {
+    search(matching: $term) {
+      ... MenuItemFields
+      ... CategoryFields
+      __typename
+    }
+  }
+
+  fragment MenuItemFields on MenuItem {
+    name
+  }
+
+  fragment CategoryFields on Category {
+    name
+    items {
+      ... MenuItemFields
+    }
+  }
+  """
+  @variables %{term: "e"}
+  test "Search, with query using fragments, returns a list of menu items and categories" do
     response = get(build_conn(), "/api", query: @query, variables: @variables)
     assert %{"data" => %{"search" => results}} = json_response(response, 200)
     assert length(results) > 0
