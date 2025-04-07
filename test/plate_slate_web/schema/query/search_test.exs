fb5546c0363b3d9d62d1f4a78389a3cf3920a536
Paul Spain <pvspain@gmail.com>
2025-04-07 14:39:18 +1000

    Add search test with cyclic fragments in query.

--- test/plate_slate_web/schema/query/search_test.exs
+++ test/plate_slate_web/schema/query/search_test.exs
@@ -50,4 +50,51 @@ defmodule PlateSlateWeb.Schema.Query.SearchTest do
     assert Enum.find(results, &(&1["__typename"] == "Category"))
     assert Enum.find(results, &(&1["__typename"] == "MenuItem"))
   end
+
+  @query """
+  query Search($term: String!) {
+    search(matching: $term) {
+      ... MenuItemFields
+      ... CategoryFields
+      ... CyclicFields
+      __typename
+    }
+  }
+
+  fragment MenuItemFields on MenuItem {
+    name
+  }
+
+  fragment CategoryFields on Category {
+    name
+    items {
+      ... MenuItemFields
+    }
+    ... CyclicFields
+  }
+
+  fragment CyclicFields on Category {
+    __typename
+    ...CategoryFields
+  }
+  """
+  @variables %{term: "e"}
+  test "Search, with query using cyclic fragments, returns a list of menu items and categories" do
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+
+    assert %{
+             "errors" => [
+               %{
+                 "locations" => [%{"column" => 1, "line" => 14}],
+                 "message" =>
+                   "Cannot spread fragment \"CategoryFields\" within itself via \"CyclicFields\", \"CategoryFields\"."
+               },
+               %{
+                 "locations" => [%{"column" => 1, "line" => 22}],
+                 "message" =>
+                   "Cannot spread fragment \"CyclicFields\" within itself via \"CategoryFields\", \"CyclicFields\"."
+               }
+             ]
+           } = json_response(response, 200)
+  end
 end
